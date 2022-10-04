fef5e29eab530a6b99232f2c25841e86aff8ead9
Yolanda Yu <yolanda@Yolandas-MacBook-Pro.local>
2022-10-03 23:05:15 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,70 @@
+import java.io.*;
+import java.io.IOException;
+import java.util.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Driver { 
- public static void main(String [] args) { 
-  Polynomial p = new Polynomial(); 
-  System.out.println(p.evaluate(3)); 
-  double [] c1 = {6,0,0,5}; 
-  Polynomial p1 = new Polynomial(c1); 
-  double [] c2 = {0,-2,0,0,-9}; 
-  Polynomial p2 = new Polynomial(c2); 
+ public static void main(String [] args)throws Exception 
+ { 
+  // Polynomial p = new Polynomial(); 
+  // System.out.println(p.evaluate(3)); 
+
+  double [] c1 = {-1, 1}; 
+  int [] e1 = {1, 2};
+  Polynomial p1 = new Polynomial(c1, e1); 
+  double [] c2 = {-7.3,-2, 3, 7.1}; 
+  int [] e2 = {0,1,9, 4};
+  Polynomial p2 = new Polynomial(c2, e2); 
+  File file = new File("");
+  String currentPath = file.getAbsolutePath();
+
+  // multiply
+  Polynomial m = p1.multiply(p2);
+  System.out.println("_________");
+  for(int i = 0; i < m.coe.length; i++){
+    System.out.println(m.coe[i]);
+  }
+  System.out.println("_________");
+  for(int i = 0; i < m.exp.length; i++){
+    System.out.println(m.exp[i]);
+  }
+
+  // add
   Polynomial s = p1.add(p2); 
-  System.out.println("s(0.1) = " + s.evaluate(0.1)); 
+  System.out.println("_________");
+  for(int i = 0; i < s.coe.length; i++){
+    System.out.println(s.coe[i]);
+  }
+  System.out.println("_________");
+  for(int i = 0; i < s.exp.length; i++){
+    System.out.println(s.exp[i]);
+  }
+
+  // evaluate&root
+  System.out.println("_________");
+  System.out.println("s(1) = " + s.evaluate(1)); 
   if(s.hasRoot(1)) 
-   System.out.println("1 is a root of s"); 
+  {
+    System.out.println("1 is a root of s"); 
+  }
   else 
-   System.out.println("1 is not a root of s"); 
- } 
-} 
+  {
+    System.out.println("1 is not a root of s"); 
+  }
+  
+  // left test
+  System.out.println("_________");
+  String path = currentPath + "/"+"test.txt";
+  p2.saveToFile(path);
+  File af = new File(path);
+  Polynomial pl = new Polynomial(af);
+  for(int z = 0; z < pl.coe.length; z++){
+    System.out.println(pl.coe[z]);
+  }
+  System.out.println("_________");
+  for(int g = 0; g < pl.exp.length; g++){
+    System.out.println(pl.exp[g]);
+  }
+ }    
+}
--- Polynomial.java
+++ Polynomial.java
@@ -1,38 +1,96 @@
+import java.io.*;
+import java.io.IOException;
+import java.util.*;
+
 public class Polynomial{
     double [] coe;
+    int [] exp;
     public Polynomial() {
         coe = new double[1];
-        coe[0]= 0;
+        exp = new int[1];
+        coe[0] = 0;
+        exp[0] = 0;
     }
-    public Polynomial(double[] arr) {
-        int size = arr.length;
+    public Polynomial(double[] c, int[] e) {
+        int size = c.length;
         coe = new double[size];
+        exp = new int[size];
         for(int i = 0; i < size; i++){
-            coe[i] = arr[i];
+            coe[i] = c[i];
+            exp[i] = e[i];
         }
     }
     public Polynomial add(Polynomial pl){
-        int size = coe.length;
-        int sizepl = pl.coe.length;
-        double[] ar = new double[Math.max(size, sizepl)];
-        
-        for(int i = 0; i < size; i++)
+        int size = exp.length;
+        int sizepl = pl.exp.length;
+        int count = size + sizepl;
+        for(int p = 0; p < size; p++)
         {
-            ar[i] += coe[i];
+            for(int q = 0; q < sizepl; q++)
+            {
+                if(exp[p] == pl.exp[q])
+                {
+                    count--;
+                }
+            }
         }
-        for(int j = 0; j < sizepl; j++)
+        double[] c;
+        int[] e;
+        c = new double[count];
+        e = new int[count];
+        int i = 0;
+        int j = 0;
+        while(i < size){
+            while(j < sizepl)
+            {
+                if(exp[i] == pl.exp[j])
+                {
+                    c[i] = pl.coe[j];
+                    break;
+                }
+                j++;
+            }
+            e[i] = exp[i];
+            c[i] += coe[i];
+            i++;
+            j = 0;
+        }
+        int ed  = count - 1;
+        int dif = 0;
+        if(c[ed] == 0)
         {
-            ar[j] += pl.coe[j];
+            while(j < sizepl)
+            {
+                i = 0;
+                dif = 0;
+                // System.out.println(ed);
+                while(i < size){
+                    
+                    if(exp[i] == pl.exp[j])
+                    {
+                        break;
+                    }
+                    dif++;
+                    i++;
+                }
+                
+                if(dif == size){
+                    c[ed] = pl.coe[j];
+                    e[ed] = pl.exp[j];
+                    ed--;
+                }
+                j++;
+            }
         }
-        Polynomial pll = new Polynomial(ar);
-        return pll;
-        
+        Polynomial pll = new Polynomial(c, e);
+        return pll;  
     }
     public double evaluate(double x){
         int size = coe.length;
         double r = 0;
         for(int i = 0; i < size; i++){
-            r += coe[i] * Math.pow(x, i);
+            int pow = exp[i];
+            r += coe[i] * Math.pow(x, pow);
         }
         return r;
     }
@@ -41,5 +99,90 @@ public class Polynomial{
         if(evaluate(x) == re)
             return true;
         return false;
+    }
+    public Polynomial multiply(Polynomial pl)
+    {
+        int i = 0;
+        int j = 0;
+        int sizepl = pl.exp.length;
+        int size = exp.length;
+        double [] c = new double[sizepl];
+        int [] e = new int[sizepl]; 
+        Polynomial pll = new Polynomial(c, e);
+        double [] cc = new double[0];
+        int [] ee = new int[0]; 
+        Polynomial ans = new Polynomial(cc, ee);
+        while(i < size)
+        {
+            while(j < sizepl)
+            {
+                pll.coe[j] = coe[i] * pl.coe[j];
+                pll.exp[j] = exp[i] + pl.exp[j];
+                
+                j++;
+            }
+            ans = ans.add(pll);
+            i++;
+            j = 0;
+        }
+        return ans;
+    }
+
+    public Polynomial (File f) throws Exception
+    {
+        Scanner scan = new Scanner(f);
+        String con = scan.nextLine();
+        scan.close();
+        String [] str = con.split("(?=[-+])", 0);
+        int size = str.length;
+        coe = new double[size];
+        exp = new int[size]; 
+        int i = 0;
+        while(i < size){
+            String [] s = str[i].split("x",2);
+            if(s.length == 1){
+                coe[i] = Double.parseDouble(s[0]);
+                exp[i] = 0;
+            }
+            else{
+                coe[i] = Double.parseDouble(s[0]);
+                exp[i] = Integer.parseInt(s[1]);
+            }
+           
+            i++;
+        }
+    }
+    public void saveToFile(String name) throws Exception{
+        String re = "";
+        int i = 0;
+        while(i < coe.length){
+            
+            if(exp[i] == 0){
+                if(coe[i] > 0){
+                    if(i != 0){
+                        re = re + "+";
+                    }
+                    re = re + coe[i];
+                }
+                else{
+                    re = re + coe[i];
+                }
+            }
+            else{
+                if(coe[i] > 0){
+                    if(i != 0){
+                        re = re + "+";
+                    }
+                    re = re + coe[i] + "x" + exp[i];
+                }
+                else{
+                    re = re + coe[i] + "x" + exp[i];
+                }
+            }
+            i++;
+        }
+        FileWriter wr = new FileWriter(name);
+        wr.write(re);
+        wr.close();
     }
 } 
